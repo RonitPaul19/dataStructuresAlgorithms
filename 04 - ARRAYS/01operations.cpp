186594d3b48d7a7be5b05037fcd34043540232f8
RonitPaul19 <ronitpaulindia@gmail.com>
2025-06-21 16:20:11 +0530

    push

--- 04 - ARRAYS/01operations.cpp
+++ 04 - ARRAYS/01operations.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout<<"Welcome to the C++ Arrays Operations Program!" << endl; 
+int insert(int arr[], int size, int element, int capacity, int index){
+    if(size >= capacity) {
+        return 0;
+    }
+    for(int i = size - 1; i >= index; i--) {
+        arr[i + 1] = arr[i];
+    }
+    arr[index] = element;
+    return 1;
+}
 
-    return 0;
+int main() {
+    int arr[100] = {1, 2, 3, 4, 5};
+    int size = 5;
+    int capacity = 100;
+    int element = 10;
+    int index = 2;
+    cout << "Array before insertion: ";
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    if(insert(arr, size, element, capacity, index)) {
+        size++;
+        cout << "Array after insertion: ";
+        for(int i = 0; i < size; i++) {
+            cout << arr[i] << " ";
+        }
+    } else {
+        cout << "Insertion failed. Array is full." << endl;
+    }
+    return 0;           
 }
