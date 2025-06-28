dbfc40ec4d5baffd1b4b025d4e9c01fb744f0c1c
RonitPaul19 <ronitpaulindia@gmail.com>
2025-06-28 15:44:48 +0530

    new

--- 04 - ARRAYS/06prog4.cpp
+++ 04 - ARRAYS/06prog4.cpp
@@ -1,1 +1,29 @@
 //left rotate the array by one place 
+#include <iostream>
+using namespace std; 
+
+void leftRotateByOne(int arr[], int n){
+    int temp = arr[0];
+    for(int i = 1; i < n; i++){
+        arr[i-1] = arr[i];
+    }
+    arr[n-1] = temp;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+
+    cout<<"Before left rotation: ";
+    for(int i = 0; i < 5; i++){
+        cout << arr[i] << " ";
+    }
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+    leftRotateByOne(arr, n);
+
+    cout << "\nAfter left rotation: ";
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
---
+++ 04 - ARRAYS/07prog5.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+void RotateRight(int arr[], int n, int k)
+{
+    if (n == 0)
+        return;
+    k = k % n;
+    if (k > n)
+        return;
+    int temp[k];
+    for (int i = n - k; i < n; i++)
+    {
+        temp[i - n + k] = arr[i];
+    }
+    for (int i = n - k - 1; i >= 0; i--)
+    {
+        arr[i + k] = arr[i];
+    }
+    for (int i = 0; i < k; i++)
+    {
+        arr[i] = temp[i];
+    }
+}
+void RotateLeft(int arr[], int n, int d)
+{
+    if (n == 0)
+        return;
+    d = d % n;
+    if (d == 0)
+        return;
+    int temp[d];
+    // Copy first d elements to temp
+    for (int i = 0; i < d; i++)
+    {
+        temp[i] = arr[i];
+    }
+    // Shift the rest of the elements to the left
+    for (int i = d; i < n; i++)
+    {
+        arr[i - d] = arr[i];
+    }
+    // Copy temp elements to the end
+    for (int i = 0; i < d; i++)
+    {
+        arr[n - d + i] = temp[i];
+    }
+}
+int main()
+{
+    int n = 7;
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int d = 2;
+    RotateLeft(arr, n, d);
+    cout << "After Rotating the elements to left by "<<d<<" places : " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    RotateRight(arr, n, d);
+    cout << "After Rotating the elements to right by "<<d<<" places : " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
