0b8023bb25ff13a054538097922fe1a1c5d95cb8
RonitPaul19 <ronitpaulindia@gmail.com>
2025-07-04 22:06:50 +0530

    Create 12prog10.cpp

---
+++ 04 - ARRAYS/12prog10.cpp
@@ -0,0 +1,20 @@
+//find a number that appears once, and othert number twice
+#include <iostream>
+using namespace std;
+
+int findOneTimeAppeared(int a[], int n){
+    int x = 0;
+    for(int i = 0; i < n; i++) {
+        x ^= a[i];
+    }
+    return x;
+}
+
+int main() {
+    int arr[] = {1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6};
+    int n=11;
+
+    int ans = findOneTimeAppeared(arr, n);
+
+    return 0;
+}
